333d78fc4f397120bda3895fbf687cbc07644a02
Bensadia Omar <bensadia.omar@gmail.com>
2023-06-20 09:47:52 +0200

    modif team dev

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+teamDev : any = [];
+
+
+ngOnInit(): void {
+
+  setTimeout(() => {
+    for (let i = 0; i < 3500 ; i++){
+      if(UserService.pokeData[i].name == 'damien'){
+       
+      this.teamDev.push(UserService.pokeData[i])
+      
+      }
+      if(UserService.pokeData[i].name == 'olivier'){
+       
+        this.teamDev.push(UserService.pokeData[i])
+        
+        }
+      if(UserService.pokeData[i].name == 'rachid'){
+       
+        this.teamDev.push(UserService.pokeData[i])
+        console.log(this.teamDev);
+        }
+      
+    }
+   
+  }
+    , 1000);
+}
+
+  
 }
