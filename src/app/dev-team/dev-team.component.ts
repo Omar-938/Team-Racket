7382de7bc45d9cbc86b69561336efa04238a4afb
moreaudamien <moreau_student2023@wilder.school>
2023-06-20 09:50:56 +0200

    mise en place de la page-dev

--- src/app/dev-team/dev-team.component.ts
+++ src/app/dev-team/dev-team.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-dev-team',
   templateUrl: './dev-team.component.html',
   styleUrls: ['./dev-team.component.css']
 })
-export class DevTeamComponent {
+export class DevTeamComponent implements OnInit
+ {
+
+  teamDev : any = [];
+
+
+ngOnInit(): void {
+
+  setTimeout(() => {
+    for (let i = 0; i < 3500 ; i++){
+      if(UserService.pokeData[i].name == 'damien'){
+
+      this.teamDev.push(UserService.pokeData[i])
+
+      }
+      if(UserService.pokeData[i].name == 'olivier'){
+
+        this.teamDev.push(UserService.pokeData[i])
+
+        }
+      if(UserService.pokeData[i].name == 'rachid'){
+
+        this.teamDev.push(UserService.pokeData[i])
+        }
+    }
+   
+
+  }
+    , 500);
+}
 
 }
